be37fd7df10d47ca41fbd90c0641482859cc4d2c
Lakisha J <lakishajohnson@pursuit.org>
2022-01-07 16:15:42 -0500

    Models file

---
+++ models/bookmark.js
@@ -0,0 +1,24 @@
+// MODULE.EXPORTS = []
+const bookmarks = [
+    {
+      name: "MDN",
+      url: "https://developer.mozilla.org/en-US/",
+      isFavorite: true,
+      category: "educational",
+    },
+    {
+      name: "Apartment Therapy",
+      url: "https://www.apartmenttherapy.com",
+      isFavorite: true,
+      category: "inspirational",
+    },
+    {
+      name: "DMV",
+      url: "https://dmv.ny.gov",
+      isFavorite: false,
+      category: "adulting",
+    },
+  ];
+
+
+  module.exports = bookmarks;
